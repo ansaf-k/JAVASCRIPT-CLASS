b7ec593d026c0a5eb3b7bdd2b314ca0969be2c37
ansaf-k <ansafk369@gmail.com>
2024-08-22 23:31:31 +0530

    challenges10

--- CHALLENGES 1/challenges10.js
+++ CHALLENGES 1/challenges10.js
@@ -29,3 +29,27 @@ const game = {
     team2: 6.5,
     },
     };
+
+    const players1 = [...game.players[0]]
+    const players2 = [...game.players[1]]
+    // console.log(players1);
+    // console.log(players2);
+
+    const gk = players1[0]
+    const fieldPlayers = players1.slice(1);
+
+    const allPlayers = [...players1,...players2]
+
+    const players1Final = [...players1,"Thiago","Coutinho","Perisic"]
+
+    // console.log(players1Final);
+
+    const team1 = game.odds.team1
+    const draw = game.odds.x
+    const team2 = game.odds.team2
+
+    const MinOdd = Math.min(team1,team2,draw)
+    console.log(MinOdd);
+    console.log(`The team most chance to win is ${MinOdd === team1 ? game.team1 : game.team2}`);
+
+    
